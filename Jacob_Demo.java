d050cf7eb879f66f38a55046d1898ba1cd463ce1
JaBaconator <jacobrispoli413@gmail.com>
2023-05-01 09:29:14 -0400

    Update Jacob_Demo.java

--- Jacob_Demo.java
+++ Jacob_Demo.java
@@ -13,8 +13,7 @@ public class Jacob_Demo
     /**
      * Constructor for objects of class Jacob_Demo
      */
-    public Jacob_Demo()
-    {
+    public Jacob_Demo() {
         // initialise instance variables
         x = 0;
     }
@@ -25,8 +24,7 @@ public class Jacob_Demo
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
+    public int sampleMethod(int y) {
         // put your code here
         return x + y;
     }
