9dc6e9138189580b95abb203a6e263be9cf9f7c1
JaBaconator <jacobrispoli413@gmail.com>
2023-10-11 11:22:15 -0400

    Changes

---
+++ makeMovie.java
@@ -0,0 +1,17 @@
+import java.util.*;
+/**
+ * Write a description of class Movies here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class makeMovie {
+    // instance variables - replace the example below with your own
+    /**
+     * Constructor for objects of class Movies
+     */
+    public void makeMovie(Movie name, ArrayList<Movie> movies) {
+        name = new Movie();
+        movies.add(name);
+    }
+}
---
+++ start_up.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Scanner;
+/**
+ * Write a description of class start_up here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class start_up
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class start_up
+     */
+    public start_up() {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
